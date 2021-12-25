c2de4dd6fc1634a77c6e6558c5814b6622c79fdc
Conrad Sollitt <csollitt@hotmail.com>
2021-12-25 02:06:17 -0800

    🖥️ Sync Script update for new Server

--- scripts/sync-server-from-github.sh
+++ scripts/sync-server-from-github.sh
@@ -7,7 +7,7 @@
 #  the author once published changes are confirmed.
 #
 #  To run:
-#      bash /var/www/scripts/sync-server-from-github.sh
+#      bash /var/www/fastsitephp-site/scripts/sync-server-from-github.sh
 #
 #  For testing with [rsync] use [-n = --dry-run]
 #  Example:
@@ -18,9 +18,9 @@
 wget https://github.com/fastsitephp/fastsitephp/archive/master.zip -O ~/master.zip
 unzip -q ~/master.zip
 rm ~/master.zip
-rsync -rcv --delete ~/fastsitephp-master/website/app/ /var/www/app
-rsync -rcv --delete --exclude .env ~/fastsitephp-master/website/app_data/ /var/www/app_data
-rsync -rcv --delete --exclude Web.config --exclude .htaccess --exclude index.php ~/fastsitephp-master/website/public/ /var/www/html
-rsync -rcv --delete ~/fastsitephp-master/scripts/ /var/www/scripts
-rsync -rcv --delete ~/fastsitephp-master/src/ /var/www/vendor/fastsitephp/src
+rsync -rcv --delete ~/fastsitephp-master/website/app/ /var/www/fastsitephp-site/app
+rsync -rcv --delete --exclude .env ~/fastsitephp-master/website/app_data/ /var/www/fastsitephp-site/app_data
+rsync -rcv --delete --exclude Web.config --exclude .htaccess --exclude index.php ~/fastsitephp-master/website/public/ /var/www/fastsitephp-site/public
+rsync -rcv --delete ~/fastsitephp-master/scripts/ /var/www/fastsitephp-site/scripts
+rsync -rcv --delete ~/fastsitephp-master/src/ /var/www/fastsitephp-site/vendor/fastsitephp/src
 rm -r ~/fastsitephp-master
